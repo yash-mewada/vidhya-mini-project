7deaa23ab886420c20213e75a0db70cef344e76b
yash-mewada <yashmewada27@gmail.com>
2023-10-08 20:00:30 +0530

    create course api implemented

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -40,8 +40,9 @@ const CreatePage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("/api/course", values);
+      const response = await axios.post("/api/courses", values);
       router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created");
     } catch {
       toast.error("Something went wrong!");
     }
