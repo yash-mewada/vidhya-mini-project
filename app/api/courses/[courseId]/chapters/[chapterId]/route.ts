0253f28d1a6a1926677832d6fac9e8e93ab54265
yash-mewada <yashmewada27@gmail.com>
2023-10-16 20:34:49 +0530

    chapter edit page implemented

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    // TODO: handle video upload
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSES_CHAPTER_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
