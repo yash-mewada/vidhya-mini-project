f15e7fa48adc7ee884e7c52303c045c6f98f35fc
yash-mewada <yashmewada27@gmail.com>
2023-11-16 11:45:20 +0530

    course data table implemented

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,28 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
